dea5cd9baf256fdf88122d382d7932ea80cec1a3
osamaaraja <osamaakbarraja@yahoo.com>
2025-07-22 22:19:06 +0200

    contains all the hyperparameters and the flags for runing specific plots

---
+++ hyperparameters.py
@@ -0,0 +1,24 @@
+days = 7
+
+K_RANGE = range(2, 16)
+SIL_THR = 0.25
+
+
+set_percentage = 1/100
+
+RUN_ALL = False
+
+RUN_FLAGS = {
+    "KMEANS": True,
+    "AGG_EUCLID": True,
+    "AGG_MANHATTAN": False,
+    "AGG_MAHALANOBIS": False,
+    "AGG_COSINE": False,
+    "AGG_EU_PRE": False,
+    "AGG_DTW_FAST": False,
+    "AGG_DTW_TS": False,
+    "BINARY": False,
+    "DBSCAN_EU": False,
+    "DBSCAN_DTW": False,
+    "SPECTRAL": False,
+}
